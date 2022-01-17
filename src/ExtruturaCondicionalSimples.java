5db8bd3b9a356cf0ad7bc1522a9a9fb0f4dd6e8e
DouglasEdson <douglas22@live.com.pt>
2022-01-17 13:34:58 -0300

    Extrutura Condicional Simples

---
+++ src/ExtruturaCondicionalSimples.java
@@ -0,0 +1,15 @@
+
+public class ExtruturaCondicionalSimples {
+	public static void main(String[] args) {
+
+		Integer x = 10;
+
+		if (x > 8) {
+			System.out.println("Bom dia");
+		}
+		System.out.println("Boa tarde");
+
+		System.out.println("Bom noite");
+	}
+
+}
